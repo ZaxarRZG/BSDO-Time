98c14150130a0ad1ae28d089df64da995d9afca8
V1tamin4ik <vitalijstalarcuk@gmail.com>
2026-03-20 00:49:28 +0300

    Add files via upload

---
+++ Theme.js
@@ -0,0 +1,26 @@
+        const body = document.body;
+        const toggleButton = document.getElementById('theme-toggle');
+        const themeSpan = document.getElementById('current-theme');
+
+
+        function setTheme(theme) {
+
+            body.classList.remove('light-theme', 'dark-theme');
+
+            body.classList.add(theme + '-theme');
+
+            themeSpan.textContent = theme === 'light' ? 'светлая' : 'тёмная';
+
+            localStorage.setItem('theme', theme);
+        }
+
+
+        const savedTheme = localStorage.getItem('theme') || 'light'; // по умолчанию светлая
+        setTheme(savedTheme);
+
+
+        toggleButton.addEventListener('click', () => {
+            const currentTheme = body.classList.contains('light-theme') ? 'light' : 'dark';
+            const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+            setTheme(newTheme);
+        });
